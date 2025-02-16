89862f9a331b5713019bf0146732121da35cab04
Quentin Heilmann <quentin002h@gmail.com>
2025-02-17 00:04:24 +0100

    feat: Enhance CraftCommand with suggestions that only appear if a recipe is available for that item.

--- app/src/main/java/me/qheilmann/vei/Command/CraftCommand.java
+++ app/src/main/java/me/qheilmann/vei/Command/CraftCommand.java
@@ -4,7 +4,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.GreedyStringArgument;
 import dev.jorel.commandapi.arguments.ItemStackArgument;
+import dev.jorel.commandapi.arguments.SafeSuggestions;
+import dev.jorel.commandapi.arguments.StringArgument;
+import me.qheilmann.vei.VanillaEnoughItems;
 import me.qheilmann.vei.Menu.MenuManager;
 
 public class CraftCommand implements ICommand{
@@ -31,7 +36,32 @@ public class CraftCommand implements ICommand{
             .withAliases(ALIASES)
             .withPermission(PERMISSION)
             .withHelp(SHORT_DESCRIPTION, LONG_DESCRIPTION)
-            .withArguments(new ItemStackArgument("item"))
+            // Display name of the item but the ItemStack back conversion behind is missing
+            // .withArguments(new StringArgument("item").replaceSuggestions(
+            //     ArgumentSuggestions.strings(info -> VanillaEnoughItems.allRecipesMap.getItems().stream().map(t -> t.getI18NDisplayName()).toArray(String[]::new)))
+            // )
+            // A try with the brigadier builder 
+            // (next time try with the builder.sugget to maybe force suggestions even if the start doesn't match (minecraft: prefix and argument))
+            // https://commandapi.jorel.dev/9.6.0/brigadiersuggestions.html?highlight=emoji#example---making-an-emoji-broadcasting-message
+            // .withArguments(new GreedyStringArgument("item").replaceSuggestions((info, builder) -> {
+            //     builder = builder.createOffset(builder.getStart() + info.currentArg().length());
+            //     builder.suggest("item");
+            // }))
+            .withArguments(new ItemStackArgument("item").replaceSafeSuggestions(
+                SafeSuggestions.suggest(info -> {
+                    return VanillaEnoughItems.allRecipesMap.getItems().stream().filter(item ->
+                        // This filter isn't really efficient: it compares the beginning of the argument with all possible elements
+                        // BUT at the same time checks with the default prefix "minecraft:", so only minecraft:minecart works for a the 5 first characters.
+                        // {
+                        //     VanillaEnoughItems.LOGGER.info("item: " + item.getType().name().toLowerCase() + " input: " + info.currentArg().toLowerCase() + " match: " + item.getType().name().toLowerCase().startsWith(info.currentArg().toLowerCase()));
+                        //     return item.getType().name().toLowerCase().startsWith(info.currentArg().toLowerCase());
+                        // }
+                        {
+                            return true;
+                        }
+                    ).toArray(ItemStack[]::new);
+                })
+            ))
             .executesPlayer((player, args) -> {
                 ItemStack itemStack = (ItemStack) args.get("item");
                 openRecipe(player, itemStack);
